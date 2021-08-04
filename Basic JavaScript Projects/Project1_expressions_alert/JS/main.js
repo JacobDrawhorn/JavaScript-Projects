d586d476285bc7b774e747c6083b52d91e566496
JacobDrawhorn <jake.drawhorn@gmail.com>
2021-08-04 15:43:24 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var A = "\"Hello, World!\"";
+window.alert(A);
+
+var B = "your";
+var C = "Awesome";
+var C = C.fontcolor("yellow");
+
+document.write(B + C);
+
+var D = 5 + 5;
+document.write(D);
