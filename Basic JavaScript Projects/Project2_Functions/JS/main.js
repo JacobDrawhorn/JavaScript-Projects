9a24c6f3f20637cbd45cb15788146306781a0161
JacobDrawhorn <jake.drawhorn@gmail.com>
2021-08-04 16:21:22 -0700

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function() {
+    var str = "This text is blue!";
+    var result = str.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I";
+    sentence += " <3 u!";
+    document.getElementById("concentrate").innerHTML = sentence;
+}
