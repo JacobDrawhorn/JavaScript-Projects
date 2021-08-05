d82b2156c64fe9cfac2dcc426740bb4673db761d
JacobDrawhorn <jake.drawhorn@gmail.com>
2021-08-05 00:15:07 -0700

    PROJECT5_COMPARISONS_TYPE_COERCION SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,13 @@
+document.write("10" + 5);
+
+document.write(typeof "Word")
+
+document.write(5 == 5) //is 5 equal to 5?
+
+document.write(5 === 5) //is 5 the same value and type of 5?
+
+document.write(5 > 2 && 4 < 100) //is 5 greater then 2 and 4 less then 100?
+
+document.write(5 > 2 || 4 > 7) //is 5 greater then 2 or 4 greater then 7?
+
+document.write(!(5>1)) //is 5 not greater then 1?
