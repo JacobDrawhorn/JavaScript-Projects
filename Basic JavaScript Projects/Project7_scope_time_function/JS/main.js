89d740c16ea6760ab14066934ac114c4c5cb7ada
JacobDrawhorn <jake.drawhorn@gmail.com>
2021-08-09 11:45:21 -0700

    Update main.js

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,14 +1,14 @@
-var X = 80;
+var X = 80; //GLOBAL VARIABLE
 var Y = 70;
 
-function get_Date() {
+function get_Date() {       //FUNCTION TO TELL YOU GOOD DAY IF IT ISNT NIGHT
     if (new Date().getHour() < 18) {
     document.getElementById("Greeting").innerHTML = "How are you today?";
     console.log("test")
     }
 }
 
-function number() {
+function number() {        //FUNCTION TO TELL WHICH GLOBAL VARIABLE IS GREATER, X OR Y
     if (X > Y) {
     document.getElementById("Greeting2").innerHTML = "X is greater then Y."
     }
@@ -17,8 +17,8 @@ function number() {
     }
 }
 
-function Time_function() {
-    var Time = new Date().getHours();
+function Time_function() {      //FUNCTION TO TELL TIME
+    var Time = new Date().getHours();   //LOCAL VARIABLE
     var Reply;
     if (Time < 12 == Time > 0) {
         Reply = "It is morning time!";
